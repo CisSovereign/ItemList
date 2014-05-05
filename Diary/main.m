6c682d6bf52cff332be60df87795b090b605ac86
Collin Hartigan <lakers4life236@yahoo.com>
2014-05-05 15:16:42 -0700

    Added Xcode project

---
+++ Diary/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Diary
+//
+//  Created by Collin Hartigan on 5/5/14.
+//  Copyright (c) 2014 Collin Hartigan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DAAppDelegate class]));
+    }
+}
