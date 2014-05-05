ee70958bf0b544e5be75fc273f4d521394ad5b73
Collin Hartigan <lakers4life236@yahoo.com>
2014-05-05 15:50:13 -0700

    Added core data entries; NS Enum mood

---
+++ Diary/DADiaryEntry.h
@@ -0,0 +1,26 @@
+//
+//  DADiaryEntry.h
+//  Diary
+//
+//  Created by Collin Hartigan on 5/5/14.
+//  Copyright (c) 2014 Collin Hartigan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+NS_ENUM(int16_t, DADiaryEntryMood){
+    DADiaryEntryMoodGood = 0,
+    DADiaryEntryMoodAverage = 1,
+    DADiaryEntryMoodBad = 2
+};
+
+@interface DADiaryEntry : NSManagedObject
+
+@property (nonatomic) NSTimeInterval date;
+@property (nonatomic, retain) NSString * body;
+@property (nonatomic, retain) NSData * imageData;
+@property (nonatomic) int16_t mood;
+@property (nonatomic, retain) NSString * location;
+
+@end
---
+++ Diary/DADiaryEntry.m
@@ -0,0 +1,20 @@
+//
+//  DADiaryEntry.m
+//  Diary
+//
+//  Created by Collin Hartigan on 5/5/14.
+//  Copyright (c) 2014 Collin Hartigan. All rights reserved.
+//
+
+#import "DADiaryEntry.h"
+
+
+@implementation DADiaryEntry
+
+@dynamic date;
+@dynamic body;
+@dynamic imageData;
+@dynamic mood;
+@dynamic location;
+
+@end
