a78c2d7abfa362b1e5fd3820361eecd2623feb01
Collin Hartigan <lakers4life236@yahoo.com>
2014-05-10 17:01:35 -0700

    Styled navigation headers

---
+++ Diary/DANavigationController.h
@@ -0,0 +1,13 @@
+//
+//  DANavigationController.h
+//  Diary
+//
+//  Created by Collin Hartigan on 5/10/14.
+//  Copyright (c) 2014 Collin Hartigan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DANavigationController : UINavigationController
+
+@end
---
+++ Diary/DANavigationController.m
@@ -0,0 +1,20 @@
+//
+//  DANavigationController.m
+//  Diary
+//
+//  Created by Collin Hartigan on 5/10/14.
+//  Copyright (c) 2014 Collin Hartigan. All rights reserved.
+//
+
+#import "DANavigationController.h"
+
+@interface DANavigationController ()
+
+@end
+
+@implementation DANavigationController
+
+-(UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
+}
+@end
